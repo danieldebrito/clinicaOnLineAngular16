6d4e8adeea8fd28cf1dc267f6aae953a0731ab11
daniel de brito <danieldebrito@outlook.com>
2023-12-14 00:52:32 -0300

    pdf historia clinica

--- src/app/client/historia-clinica/historia-clinica.component.ts
+++ src/app/client/historia-clinica/historia-clinica.component.ts
@@ -6,6 +6,9 @@ import { EEstadoTurno, Turno } from 'src/app/class/turno';
 import { Paciente } from 'src/app/class/usuarios/paciente';
 import { turnosService } from 'src/app/services/turnos.service';
 
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+
 @Component({
   selector: 'app-historia-clinica',
   templateUrl: './historia-clinica.component.html',
@@ -30,19 +33,79 @@ export class HistoriaClinicaComponent implements OnInit {
     return date.toISOString();
   }
 
+  generatePDF() {
+    const pdf = new jsPDF();
+
+    // Position of the image
+    pdf.addImage('assets/logo/logo.png', 'PNG', 10, 10, 40, 10);
+
+    // Set font size and type for the title
+    pdf.setFontSize(16);
+    pdf.setFont('helvetica', 'bold');
+    // Add title to the document
+    pdf.text('Historia Clínica', 90, 20);
+    ///y   x   ejes
+
+    // Reset font size and type for the rest of the document
+    pdf.setFontSize(12);
+    pdf.setFont('helvetica', 'normal');
+
+    // Data of the patient
+    const pacienteInfo = `
+  DATOS DEL PACIENTE : 
+      Nombre: ${this.paciente.nombre} ${this.paciente.apellido}
+      Email: ${this.paciente.email}
+      DNI: ${this.paciente.dni}  Fecha de Nacimiento: ${this.paciente.fechaNacimiento}
+    `;
+    pdf.text(pacienteInfo, 40, 30);
+
+    // List of turns
+    let yPosition = 50;
+
+    for (const turno of this.turnosPaciente) {
+      let turnoInfo = `
+      ___________________________________________________________________________________________________________________________
+  
+        Fecha Atención: ${turno.fechaHora.dia} / ${turno.fechaHora.mes} / ${turno.fechaHora.year}
+        Especialista: ${turno.especialista.nombre}  ${turno.especialista.apellido} Especialidad: ${turno.especialidad.nombre}
+        
+        Detalle de Atención : 
+        * Altura : ${turno.atencionPaciente.altura} cm           * Temperatura : ${turno.atencionPaciente.temperatura} ºC
+        * Presión : ${turno.atencionPaciente.presion} mm Hg      * Peso : ${turno.atencionPaciente.peso} kg
+  
+        Reseña Medica : ${turno.atencionPaciente.resena}
+      `;
+      //  información dinámica
+      if (
+        turno.atencionPaciente.dinamicos &&
+        turno.atencionPaciente.dinamicos.length > 0
+      ) {
+        turnoInfo += '\n          Datos Adicionales:';
+        for (const dinamico of turno.atencionPaciente.dinamicos) {
+          turnoInfo += `\n        * ${dinamico.dinamicoKey} : ${dinamico.dinamicoValue}`;
+        }
+      }
+
+      pdf.text(turnoInfo, 10, yPosition);
+      yPosition += 80; // separación entre turnos
+    }
+
+    // Save the PDF
+    pdf.save('historia_clinica.pdf');
+  }
+
   // USUARIOS //////////////////////////////////////////////////////////////////////////////////
   private getCurrentUser() {
     this.afAuth.authState.subscribe((user) => {
       if (user) {
         this.usuariosSv.getItemById(user.uid).subscribe((res) => {
           if (res.role == ERole.paciente) {
-
             this.turnosSv.turnoPaciente = res;
             this.paciente = res;
 
             this.turnosSv.getItems().subscribe((res) => {
               const turnos: Turno[] = res;
-        
+
               if (this.turnosSv.turnoPaciente) {
                 this.turnosPaciente = turnos.filter(
                   (e) =>
@@ -50,10 +113,7 @@ export class HistoriaClinicaComponent implements OnInit {
                     e.estado == EEstadoTurno.cumplido
                 );
               }
-        
             });
-
-
           }
         });
       } else {
@@ -67,6 +127,8 @@ export class HistoriaClinicaComponent implements OnInit {
     this.turnosSv.getItems().subscribe((res) => {
       const turnos: Turno[] = res;
 
+      console.table(turnos);
+
       if (this.turnosSv.turnoPaciente) {
         this.turnosPaciente = turnos.filter(
           (e) =>
@@ -74,7 +136,6 @@ export class HistoriaClinicaComponent implements OnInit {
             e.estado == EEstadoTurno.cumplido
         );
       }
-
     });
   }
 }
